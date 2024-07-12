34339b855b82845522ff009fcee5df60a10c11a6
PRO <cheikhmbengue571@gmail.com>
2024-07-12 15:40:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from "react";
+import { useEffect } from 'react';
 
-function App() {
+import Eleve from './Components/Eleve/Eleve';
+
+
+
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1 className="text-center">
+      Hello world!
+    </h1>
+    
+  )
+
+  useEffect(()=> {
+    console.log('[App.js useEffect]');
+
+  });
 }
 
 export default App;
